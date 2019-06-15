389dcfa6e84129b2f0c1b4813d51e40a23f7f524
a2chan <aadeshchandra01@gmail.com>
2019-06-15 21:20:52 +0530

    added what's new carousel

--- js/index.js
+++ js/index.js
@@ -232,10 +232,77 @@ function polyfillKey() {
 // top nav bar begins
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
-	var x = document.getElementsByClassName("container");
+	var x = document.getElementById("mytopnav");
 	if (x.className === "topnav") {
 		x.className += " responsive";
 	} else {
 		x.className = "topnav";
 	}
 }
+
+// carousel
+var timer = 4000;
+
+var i = 0;
+var max = $("#carousel > li").length;
+
+$("#carousel > li")
+	.eq(i)
+	.addClass("active")
+	.css("left", "0");
+$("#carousel > li")
+	.eq(i + 1)
+	.addClass("active")
+	.css("left", "25%");
+$("#carousel > li")
+	.eq(i + 2)
+	.addClass("active")
+	.css("left", "50%");
+$("#carousel > li")
+	.eq(i + 3)
+	.addClass("active")
+	.css("left", "75%");
+
+setInterval(function() {
+	$("#carousel > li").removeClass("active");
+
+	$("#carousel > li")
+		.eq(i)
+		.css("transition-delay", "0.25s");
+	$("#carousel > li")
+		.eq(i + 1)
+		.css("transition-delay", "0.5s");
+	$("#carousel > li")
+		.eq(i + 2)
+		.css("transition-delay", "0.75s");
+	$("#carousel > li")
+		.eq(i + 3)
+		.css("transition-delay", "1s");
+
+	if (i < max - 4) {
+		i = i + 4;
+	} else {
+		i = 0;
+	}
+
+	$("#carousel > li")
+		.eq(i)
+		.css("left", "0")
+		.addClass("active")
+		.css("transition-delay", "1.25s");
+	$("#carousel > li")
+		.eq(i + 1)
+		.css("left", "25%")
+		.addClass("active")
+		.css("transition-delay", "1.5s");
+	$("#carousel > li")
+		.eq(i + 2)
+		.css("left", "50%")
+		.addClass("active")
+		.css("transition-delay", "1.75s");
+	$("#carousel > li")
+		.eq(i + 3)
+		.css("left", "75%")
+		.addClass("active")
+		.css("transition-delay", "2s");
+}, timer);
