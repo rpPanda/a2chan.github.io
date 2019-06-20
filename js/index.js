bcf0c652ba013810c7910404e7421162aa6d3a22
a2chan <aadeshchandra01@gmail.com>
2019-06-20 13:34:56 +0530

    added animations to elements, ready to merge

--- js/index.js
+++ js/index.js
@@ -244,21 +244,30 @@ const navSlide = () => {
 	const burger = document.querySelector(".topnav__burger");
 	const nav = document.querySelector(".topnav__nav-links");
 	const resmenu = document.querySelector(".res-menu");
-	const navLinks = document.querySelectorAll(".topnav__nav-links li");
+	const navLinks = document.querySelectorAll(".res-menu__red li");
+	const langLinks = document.querySelectorAll(".res-menu__lang li");
 	// toggle nav
 	burger.addEventListener("click", () => {
 		nav.classList.toggle("nav-active");
 		burger.classList.toggle("open");
 		resmenu.classList.toggle("res-menu--active");
 
-		// navLinks.forEach(function(link, index) {
-		// 	time = index / 5 + 0.3;
-		// 	if (link.style.animation) {
-		// 		link.style.animation = "";
-		// 	} else {
-		// 		link.style.animation = "navLinkFade 0.5s ease forwards " + time + "s";
-		// 	}
-		// });
+		navLinks.forEach(function(link, index) {
+			time = index / 5 + 0.3;
+			if (link.style.animation) {
+				link.style.animation = "";
+			} else {
+				link.style.animation = "navLinkFade 0.5s ease forwards " + time + "s";
+			}
+		});
+		langLinks.forEach(function(link, index) {
+			time = index / 5 + 0.3;
+			if (link.style.animation) {
+				link.style.animation = "";
+			} else {
+				link.style.animation = "langLinkFade 0.5s ease forwards " + time + "s";
+			}
+		});
 	});
 };
 
