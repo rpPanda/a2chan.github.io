7725bd5628460df290db93faea53c4b012a216f4
a2chan <aadeshchandra01@gmail.com>
2019-06-20 16:44:29 +0530

    added testimonial carousel

--- js/index.js
+++ js/index.js
@@ -343,3 +343,13 @@ setInterval(function() {
 // window.onscroll = function() {
 // 	scrollProgressBar();
 // };
+if ($(".ts-testimonial-slide").length > 0) {
+	$(".ts-testimonial-slide").owlCarousel({
+		autoPlay: 4000,
+		slideSpeed: 1000,
+		navigation: false,
+		pagination: true,
+		singleItem: true,
+		items: 1
+	});
+}
