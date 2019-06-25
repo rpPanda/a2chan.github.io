fecd9e88d1224f7f495f6744292865074fcc0b73
a2chan <aadeshchandra01@gmail.com>
2019-06-25 21:11:39 +0530

    added client, writer modules (not-resposive yet)

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 // falling letters begin
-playGame();
-polyfillKey();
+// playGame();
+// polyfillKey();
 
 function playGame(replay) {
 	var LETTERS = [
@@ -113,19 +113,19 @@ function playGame(replay) {
 		animation.playbackRate = rate;
 
 		//If an animation finishes, we will consider that as a miss, so we will remove it from the active animations array and increment our miss count
-		animation.onfinish = function(e) {
+		animation.onfinish = function (e) {
 			var target = container;
 			var char = target.textContent;
 		};
 	}
 
 	//Periodically remove missed elements, and lower the interval between falling elements
-	var cleanupInterval = setInterval(function() {
+	var cleanupInterval = setInterval(function () {
 		// timeOffset = timeOffset * 4 / 5;
 		cleanup();
 	}, 20000);
 	function cleanup() {
-		[].slice.call(main.querySelectorAll(".missed")).forEach(function(missed) {
+		[].slice.call(main.querySelectorAll(".missed")).forEach(function (missed) {
 			main.removeChild(missed);
 		});
 	}
@@ -194,7 +194,7 @@ function playGame(replay) {
 	function setupNextLetter() {
 		if (gameOn) {
 			create();
-			setTimeout(function() {
+			setTimeout(function () {
 				setupNextLetter();
 			}, timeOffset);
 		}
@@ -219,7 +219,7 @@ function polyfillKey() {
 		keys[i] = letter.toLowerCase();
 	}
 	var proto = {
-		get: function(x) {
+		get: function (x) {
 			var key = keys[this.which || this.keyCode];
 			console.log(key);
 			return key;
@@ -243,7 +243,7 @@ const navSlide = () => {
 		burger.classList.toggle("open");
 		resmenu.classList.toggle("res-menu--active");
 
-		navLinks.forEach(function(link, index) {
+		navLinks.forEach(function (link, index) {
 			time = index / 5 + 0.3;
 			if (link.style.animation) {
 				link.style.animation = "";
@@ -251,7 +251,7 @@ const navSlide = () => {
 				link.style.animation = "navLinkFade 0.5s ease forwards " + time + "s";
 			}
 		});
-		langLinks.forEach(function(link, index) {
+		langLinks.forEach(function (link, index) {
 			time = index / 5 + 0.3;
 			if (link.style.animation) {
 				link.style.animation = "";
@@ -293,7 +293,7 @@ if (s_size > 900) {
 		.addClass("active")
 		.css("left", "75%");
 
-	setInterval(function() {
+	setInterval(function () {
 		$("#carousel > li").removeClass("active");
 
 		$("#carousel > li")
@@ -346,7 +346,7 @@ if (s_size > 900) {
 		.addClass("active")
 		.css("left", "50%");
 
-	setInterval(function() {
+	setInterval(function () {
 		$("#carousel > li").removeClass("active");
 
 		$("#carousel > li")
@@ -384,3 +384,77 @@ if ($(".ts-testimonial-slide").length > 0) {
 		items: 1
 	});
 }
+
+// Intersection observer
+
+const desc_card = document.querySelectorAll(".desc-container > div");
+const writer_title = document.querySelector(".writer .desc .title");
+const client_title = document.querySelector(".client .desc .title");
+const writer_text = document.querySelector(".writer .desc .text");
+const client_text = document.querySelector(".client .desc .text");
+const portfolio = document.querySelector(".portfolio");
+
+const options = {
+	root: null,
+	threshold: 0,
+	rootMargin: "-100px"
+};
+
+const observer = new IntersectionObserver(function (entries, observer) {
+	entries.forEach(entry => {
+		if (!entry.isIntersecting) {
+			return;
+		}
+		if (entry.target.className == "writer animated") {
+			entry.target.classList.toggle("fadeInLeft");
+			writer_title.style.animation = "padding_10px_yellow 1s ease-in-out 1.5s forwards";
+			writer_text.style.animation = "bg_lyellow 1s ease-in-out 1.5s forwards";
+		}
+		else {
+			entry.target.classList.toggle("fadeInRight");
+			client_title.style.animation = "padding_10px_blue 1s ease 1.5s forwards";
+			client_text.style.animation = "bg_lblue 1s ease-in-out 1.5s forwards";
+		}
+		observer.unobserve(entry.target);
+	});
+}, options);
+
+desc_card.forEach(card => {
+	observer.observe(card);
+});
+
+const one = document.querySelector(".portfolio div:nth-child(1)");
+const two = document.querySelector(".portfolio div:nth-child(2)");
+const three = document.querySelector(".portfolio div:nth-child(3)");
+const four = document.querySelector(".portfolio div:nth-child(4)");
+const five = document.querySelector(".portfolio div:nth-child(5)");
+const six = document.querySelector(".portfolio div:nth-child(6)");
+const seven = document.querySelector(".portfolio div:nth-child(7)");
+const eight = document.querySelector(".portfolio div:nth-child(8)");
+const nine = document.querySelector(".portfolio div:nth-child(9)");
+const ten = document.querySelector(".portfolio div:nth-child(10)");
+const eleven = document.querySelector(".portfolio div:nth-child(11)");
+const twelve = document.querySelector(".portfolio div:nth-child(12)");
+
+const portfolio_observer = new IntersectionObserver(function (entries, portfolio_observer) {
+	entries.forEach(entry => {
+		if (!entry.isIntersecting) {
+			return;
+		}
+		one.classList.toggle("fadeInLeft");
+		five.classList.toggle("fadeInLeft");
+		nine.classList.toggle("fadeInLeft");
+		four.classList.toggle("fadeInRight");
+		eight.classList.toggle("fadeInRight");
+		twelve.classList.toggle("fadeInRight");
+		two.classList.toggle("fadeInDown");
+		three.classList.toggle("fadeInDown");
+		ten.classList.toggle("fadeInUp");
+		eleven.classList.toggle("fadeInUp");
+		six.classList.toggle("fadeIn");
+		seven.classList.toggle("fadeIn");
+		portfolio_observer.unobserve(entry.target);
+	});
+}, options);
+
+portfolio_observer.observe(portfolio);
