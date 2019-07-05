6b67efdf3be979a19b3d42115a0007e64ad9f397
a2chan <aadeshchandra01@gmail.com>
2019-07-06 00:48:39 +0530

    made text responsive

--- js/portfolio.js
+++ js/portfolio.js
@@ -47,34 +47,69 @@ const navSlide = () => {
 
 navSlide();
 
-$(document).ready(function() {
-    $("#pagepiling").pagepiling({
-        menu: null,
-        direction: "vertical",
-        verticalCentered: true,
-        sectionsColor: ["#ffffff", "#b2ff59", "#03a9f4", "#ffeb3b"],
-        anchors: [],
-        scrollingSpeed: 500,
-        easing: "swing",
-        loopBottom: false,
-        loopTop: false,
-        css3: true,
-        navigation: {
-            textColor: "#000",
-            bulletsColor: "#000",
-            position: "right",
-            tooltips: ["section1", "section2", "section3", "section4"]
-        },
-        normalScrollElements: null,
-        normalScrollElementTouchThreshold: 5,
-        touchSensitivity: 5,
-        keyboardScrolling: true,
-        sectionSelector: ".section",
-        animateAnchor: false,
+var mq = window.matchMedia("(max-width: 570px)");
+if (mq.matches) {
+    $(document).ready(function() {
+        $("#pagepiling").pagepiling({
+            menu: null,
+            direction: "vertical",
+            verticalCentered: true,
+            sectionsColor: ["#ffffff", "#b2ff59", "#03a9f4", "#ffeb3b"],
+            anchors: [],
+            scrollingSpeed: 500,
+            easing: "swing",
+            loopBottom: false,
+            loopTop: false,
+            css3: true,
+            navigation: {
+                textColor: "#000",
+                bulletsColor: "#000",
+                position: "bottom",
+                tooltips: ["section1", "section2", "section3", "section4"]
+            },
+            normalScrollElements: null,
+            normalScrollElementTouchThreshold: 5,
+            touchSensitivity: 5,
+            keyboardScrolling: true,
+            sectionSelector: ".section",
+            animateAnchor: false,
 
-        //events
-        onLeave: function(index, nextIndex, direction) {},
-        afterLoad: function(anchorLink, index) {},
-        afterRender: function() {}
+            //events
+            onLeave: function(index, nextIndex, direction) {},
+            afterLoad: function(anchorLink, index) {},
+            afterRender: function() {}
+        });
     });
-});
+} else {
+    $(document).ready(function() {
+        $("#pagepiling").pagepiling({
+            menu: null,
+            direction: "vertical",
+            verticalCentered: true,
+            sectionsColor: ["#ffffff", "#b2ff59", "#03a9f4", "#ffeb3b"],
+            anchors: [],
+            scrollingSpeed: 500,
+            easing: "swing",
+            loopBottom: false,
+            loopTop: false,
+            css3: true,
+            navigation: {
+                textColor: "#000",
+                bulletsColor: "#000",
+                position: "right",
+                tooltips: ["section1", "section2", "section3", "section4"]
+            },
+            normalScrollElements: null,
+            normalScrollElementTouchThreshold: 5,
+            touchSensitivity: 5,
+            keyboardScrolling: true,
+            sectionSelector: ".section",
+            animateAnchor: false,
+
+            //events
+            onLeave: function(index, nextIndex, direction) {},
+            afterLoad: function(anchorLink, index) {},
+            afterRender: function() {}
+        });
+    });
+}
