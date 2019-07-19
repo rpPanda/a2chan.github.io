94409c715e023398d864bc4a06e4dc63ee7f057b
a2chan <aadeshchandra01@gmail.com>
2019-07-19 21:50:54 +0530

    changed emphasis + responsive

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 // falling letters begin
-playGame()
-polyfillKey()
+// playGame()
+// polyfillKey()
 
 function playGame(replay) {
     var LETTERS = [
@@ -347,49 +347,10 @@ const observer = new IntersectionObserver(function(entries, observer) {
     })
 }, options)
 
-desc_card.forEach(card => {
-    observer.observe(card)
-})
-
-// const one = document.querySelector(".portfolio .card:nth-child(1)")
-// const two = document.querySelector(".portfolio .card:nth-child(2)")
-// const three = document.querySelector(".portfolio .card:nth-child(3)")
-// const four = document.querySelector(".portfolio .card:nth-child(4)")
-// const five = document.querySelector(".portfolio .card:nth-child(5)")
-// const six = document.querySelector(".portfolio .card:nth-child(6)")
-// const seven = document.querySelector(".portfolio .card:nth-child(7)")
-// const eight = document.querySelector(".portfolio .card:nth-child(8)")
-// const nine = document.querySelector(".portfolio .card:nth-child(9)")
-// const ten = document.querySelector(".portfolio .card:nth-child(10)")
-// const eleven = document.querySelector(".portfolio .card:nth-child(11)")
-// const twelve = document.querySelector(".portfolio .card:nth-child(12)")
-
-// const portfolio_observer = new IntersectionObserver(function(
-//     entries,
-//     portfolio_observer
-// ) {
-//     entries.forEach(entry => {
-//         if (!entry.isIntersecting) {
-//             return
-//         }
-//         one.classList.toggle("fadeInLeft")
-//         five.classList.toggle("fadeInLeft")
-//         nine.classList.toggle("fadeInLeft")
-//         four.classList.toggle("fadeInRight")
-//         eight.classList.toggle("fadeInRight")
-//         twelve.classList.toggle("fadeInRight")
-//         two.classList.toggle("fadeInDown")
-//         three.classList.toggle("fadeInDown")
-//         ten.classList.toggle("fadeInUp")
-//         eleven.classList.toggle("fadeInUp")
-//         six.classList.toggle("fadeIn")
-//         seven.classList.toggle("fadeIn")
-//         portfolio_observer.unobserve(entry.target)
-//     })
-// },
-// options)
-
-// portfolio_observer.observe(portfolio)
+// TODO activate intersection observer
+// desc_card.forEach(card => {
+//     observer.observe(card)
+// })
 
 function addingImages() {
     for (var i = 1; i <= 91; i++) {
