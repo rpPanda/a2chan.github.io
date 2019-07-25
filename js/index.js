8b94b52b6fc93cf6b4100e17eecc465042a66d6a
a2chan <aadeshchandra01@gmail.com>
2019-07-26 02:14:24 +0530

    added sliding get a quote

--- js/index.js
+++ js/index.js
@@ -272,6 +272,10 @@ const navSlide = () => {
     }
 }
 
+const toggleForm = () => {
+    document.querySelector(".query-form").classList.toggle("showForm")
+}
+
 $(".owl-carousel.carousel").owlCarousel({
     loop: true,
     margin: 10,
@@ -390,9 +394,53 @@ function observe_topnav(x) {
 var x = window.matchMedia("(min-width: 1200px)")
 x.addListener(observe_topnav)
 
+//inlinging svg
+jQuery("img.svg").each(function() {
+    var $img = jQuery(this)
+    var imgID = $img.attr("id")
+    var imgClass = $img.attr("class")
+    var imgURL = $img.attr("src")
+
+    jQuery.get(
+        imgURL,
+        function(data) {
+            // Get the SVG tag, ignore the rest
+            var $svg = jQuery(data).find("svg")
+
+            // Add replaced image's ID to the new SVG
+            if (typeof imgID !== "undefined") {
+                $svg = $svg.attr("id", imgID)
+            }
+            // Add replaced image's classes to the new SVG
+            if (typeof imgClass !== "undefined") {
+                $svg = $svg.attr("class", imgClass + " replaced-svg")
+            }
+
+            // Remove any invalid XML tags as per http://validator.w3.org
+            $svg = $svg.removeAttr("xmlns:a")
+
+            // Check if the viewport is set, if the viewport is not set the SVG wont't scale.
+            if (
+                !$svg.attr("viewBox") &&
+                $svg.attr("height") &&
+                $svg.attr("width")
+            ) {
+                $svg.attr(
+                    "viewBox",
+                    "0 0 " + $svg.attr("height") + " " + $svg.attr("width")
+                )
+            }
+
+            // Replace image with new SVG
+            $img.replaceWith($svg)
+        },
+        "xml"
+    )
+})
+
 $(document).ready(() => {
     // fallingLetters()
-    footer_observer.observe(footer)
+    // footer_observer.observe(footer)
     observe_topnav(x)
     // })
 
