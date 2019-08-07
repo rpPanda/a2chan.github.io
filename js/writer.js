f33b9dc0d1e386faf9a11db04662688e09da8b7a
a2chan <aadeshchandra01@gmail.com>
2019-08-07 20:06:54 +0530

    added slider

--- js/writer.js
+++ js/writer.js
@@ -41,3 +41,38 @@ jQuery("img.svg").each(function() {
         "xml"
     )
 })
+
+function addingImages() {
+    for (var i = 1; i <= 15; i++) {
+        var image = document.createElement("img")
+        var slide = document.createElement("div")
+        slide.classList.add("slide", "item")
+        image.setAttribute("src", "../assets/logos/" + i + ".webp")
+        slide.appendChild(image)
+        document.querySelector(".slide-track").appendChild(slide)
+    }
+}
+
+addingImages()
+
+$(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    dots: false,
+    autoplay: true,
+    slideTransition: "linear",
+    autoplayTimeout: 5000,
+    autoplaySpeed: 5000,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 7
+        }
+    }
+})
